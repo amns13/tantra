c3477c839f5240dd82a398b90f800ed782aba34c
slimmsady <slimmsady@gmail.com>
2021-11-28 18:09:19 +0530

    added likes tables

--- apps/post/models.py
+++ apps/post/models.py
@@ -24,10 +24,11 @@ class Post(BaseModel):
 
 
 class Comment(BaseModel):
+    """Base models for comments."""
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='%(class)s')
+        related_name='%(class)ss')
     body = models.TextField('body', help_text="Contents of the comment.")
 
     class Meta:
@@ -35,6 +36,7 @@ class Comment(BaseModel):
 
 
 class PostComment(Comment):
+    """Model for comments on posts."""
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
@@ -45,3 +47,35 @@ class PostComment(Comment):
 
     def __str__(self) -> str:
         return f"{self.post}: {self.pk}"
+
+
+class Like(BaseModel):
+    """Base model for likes"""
+    id = models.BigAutoField('Unique ID', primary_key=True, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="%(class)ss")
+
+    class Meta:
+        abstract = True
+
+
+class PostLike(Like):
+    """Likes on the post"""
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes")
+
+    class Meta:
+        db_table = 'post_like'
+
+
+class PostCommentLike(Like):
+    """Likes on the comment"""
+    comment = models.ForeignKey(
+        PostComment, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        db_table = 'postcomment_like'
