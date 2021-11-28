839b60cc5ea751aeca9a5db0f4500ebfd1a6bc6d
slimmsady <slimmsady@gmail.com>
2021-11-28 20:03:57 +0530

    Updated admin file to auto generated admin file by django extension

--- apps/post/admin.py
+++ apps/post/admin.py
@@ -1,20 +1,68 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Post, PostComment
+
+from .models import Post, PostComment, PostLike, PostCommentLike
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
+        'created_at',
+        'updated_at',
+        'is_active',
         'title',
+        'body',
         'author',
     )
+    list_filter = ('created_at', 'updated_at', 'is_active', 'author')
+    date_hierarchy = 'created_at'
 
 
+@admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
+        'created_at',
+        'updated_at',
+        'is_active',
+        'author',
+        'body',
         'post',
-        'author'
     )
+    list_filter = ('created_at', 'updated_at', 'is_active', 'author', 'post')
+    date_hierarchy = 'created_at'
 
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(PostComment, PostCommentAdmin)
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    list_display = (
+        'created_at',
+        'updated_at',
+        'is_active',
+        'id',
+        'user',
+        'post',
+    )
+    list_filter = ('created_at', 'updated_at', 'is_active', 'user', 'post')
+    date_hierarchy = 'created_at'
+
+
+@admin.register(PostCommentLike)
+class PostCommentLikeAdmin(admin.ModelAdmin):
+    list_display = (
+        'created_at',
+        'updated_at',
+        'is_active',
+        'id',
+        'user',
+        'comment',
+    )
+    list_filter = (
+        'created_at',
+        'updated_at',
+        'is_active',
+        'user',
+        'comment',
+    )
+    date_hierarchy = 'created_at'
