ea5185511f0f4d1482382d3458a5e5713b87711f
slimmsady <slimmsady@gmail.com>
2021-12-10 08:50:36 +0530

    fixed bug in custom base admin model

--- core/admin.py
+++ core/admin.py
@@ -10,7 +10,7 @@ class CustomModelAdmin(admin.ModelAdmin):
     def save_model(self, request, instance, form, change):
         user = request.user
         instance = form.save(commit=False)
-        if not change or not instance.created_by:
+        if not change:
             instance.created_by = user
         instance.updated_by = user
         instance.save()
